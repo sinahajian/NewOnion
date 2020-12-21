ccf8d782a4efd16a563ad6f4cd520519db404faa
sina hajian <sina.hajian@gmail.com>
2020-12-22 01:53:30 +0330

    Create First Model

---
+++ Solution1/Hiva.Domain/Models/ProgramingMajor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hiva.Domain.Models
+{
+    public class ProgramingMajor
+    {
+        public int Id { get; set; }
+        public string Lang { get; set; }
+        public string Envirement { get; set; }
+    }
+}
